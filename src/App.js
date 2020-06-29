39e2507e776cb792ee432bd3b808cf10bc19ae85
unknown <srini.upwork@gmail.com>
2020-06-29 07:29:43 +0530

    1 MUI INTRO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button,Typography } from '@material-ui/core'
+import { ThemeProvider, makeStyles} from '@material-ui/core/styles'
+
+import theme from './theme';
+
+const useStyles = makeStyles({
+  helloThereStyles : {
+
+    fontStyle: 'oblique',
+    color : 'grey'
+  },
+  buttonStyles : {
+    color :'brown',
+    border : 0
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
+    <ThemeProvider theme={theme}>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Typography className={classes.helloThereStyles} variant='h1' color="secondary"> Hello There </Typography>
+      <Button color="primary" variant='outlined'> This is from material</Button>
     </div>
+    </ThemeProvider>
   );
 }
 
