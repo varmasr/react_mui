7541bbc3f71262eb1fc3758607fdd5ec130ab6c6
unknown <srini.upwork@gmail.com>
2020-07-01 21:52:09 +0530

    5 - Typography

--- src/App.js
+++ src/App.js
@@ -1,33 +1,69 @@
 import React from 'react';
-import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/styles';
+import TypographyDemo from './TypographyDemo';
+import {ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import blue from '@material-ui/core/colors/blue'
+//import { makeStyles } from '@material-ui/styles';
+//import Typography from '@material-ui/core/Typography';
+//import classNames from 'classnames'
 
-const useStyles = makeStyles(theme => ({
-  root : {
-    color : "red",
-    border : "none"
+const theme = createMuiTheme (
+  {
+    palette : {
+      primary : purple,
+      secondary : blue
+    },
+    typography:{
+      h1: {
+        fontSize:"1em"
+      },
+      
+      fontFamily:"Roboto"
+    }
   }
-}));
+)
+
+
+
+/* const useStyles = makeStyles({
+  typographyStyle: {
+    color: "blue"
+  },
+  gutterBottom : {
+    marginBottom : "0.6em"
+  }
+
+}); */
+
 
 function App() {
-  const classes = useStyles();
-  return (
-    <>
-{/*     <Button className={classes.root} variant="outlined" size="small" color="primary">
-      small button
-    </Button> */}
-     <Button
-      classes={{
-        root: classes.root
-     }} 
-     variant="outlined"
-     size="small"
-     color="primary">
-      small button
-    </Button>
-    
-    </>
-  );
-}
+  //const classes = useStyles();
+/*   return(
+    <Typography 
+    //align="center"
+    //color="secondary" 
+    gutterBottom 
+    //nowrap
+    //variantMapping={{
+    //  h1:"p"
+    //}}
+    //component
+    classes={{
+      gutterBottom:classes.gutterBottom,
+    }}
+    variant="h1"
+    className={classes.typographyStyle}
+    >Hello World</Typography>
+  ); */
+ 
+ return (
+   <ThemeProvider theme={theme}>
+     <TypographyDemo/>
+   </ThemeProvider>
+   
+ );
+
+}; 
 
 export default App;
---
+++ src/TypographyDemo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default function App() {
+   return( <Typography variant="h2" color="secondary">
+        Hello World
+    </Typography>
+   );
+};
